d20cbe3b7100e947e6910f39dc05c0f58752e859
susmit31 <gohanmysticdbz@gmail.com>
2022-03-04 10:19:16 +0600

    Accept command line argument for query country

--- getnews.js
+++ getnews.js
@@ -1,24 +1,26 @@
 const pup = require('puppeteer');
 const cp = require('child_process');
 
-const NO_GAME = "Sorry, no games today";
+const NO_GAME = "Sorry, no games today";    
+const QUERY_COUNTRY = process.argv.length > 2? process.argv[2] : "BAN";
 
-const getnews = async()=>{
+const getnews = async(query_country)=>{
     const browser = await pup.launch();
     const pg = await browser.newPage();
     await pg.goto('https://www.espncricinfo.com');
 
-    outputs = await pg.evaluate(()=>{
+    outputs = await pg.evaluate((country)=>{
         let matches = document.querySelectorAll('div.match-info');
-        let bd_matches = Array.from(matches).filter(team=>team.innerText.search("BAN")!=-1);
-        return bd_matches.map(game =>{
+        let team_matches = Array.from(matches).filter(team=>team.innerText.search(country)!=-1);
+        return team_matches.map(game =>{
             let teams = Array.from(game.querySelectorAll('.team')).map(team=>team.innerText);
             teams = teams.map(team=>team.split('\n')).map(team=>team.filter(x=>x!="").join(" "));
             let scores = Array.from(game.querySelectorAll('.score')).map(score=>score.innerText);
             let result = game.querySelector('.status-text').innerText;
             return {teams:teams, scores:scores, result:result};
         })
-    })
+    }, query_country)
+
     outputs.forEach(output=>{
         output.teams.forEach(team=>{
             console.log(team);
@@ -30,4 +32,4 @@ const getnews = async()=>{
     process.exit();
 }
 
-getnews();
+getnews(QUERY_COUNTRY);
